0afc2b2e5689f1f7c1d5eb4ebff914762436a551
xlvxn <alvin.fauzi.aulia@gmail.com>
2021-12-21 11:31:17 +0800

    simple football club finder

---
+++ src/script/view/main.js
@@ -0,0 +1,26 @@
+import '../component/club-list.js';
+import '../component/search-bar.js';
+import DataSource from '../data/data-source.js';
+
+const main = () => {
+    const searchElement = document.querySelector("search-bar");
+    const clubListElement = document.querySelector("club-list");
+
+    const onButtonSearchClicked = () => {
+        DataSource.searchClub(searchElement.value)
+            .then(renderResult)
+            .catch(fallbackResult)
+    };
+
+    const renderResult = results => {
+        clubListElement.clubs = results;
+    };
+
+    const fallbackResult = message => {
+        clubListElement.renderError(message);
+    };
+
+    searchElement.clickEvent = onButtonSearchClicked;
+};
+
+export default main;
